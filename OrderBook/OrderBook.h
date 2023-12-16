fb164efd0211fda1aa3c0ae8d2f0aaddc94dd1e5
Malith-19 <malith.19@cse.mrt.ac.lk>
2023-12-16 22:30:28 +0530

    Add transactions to add orders.

--- OrderBook/OrderBook.h
+++ OrderBook/OrderBook.h
@@ -41,8 +41,10 @@ public:
         numBuyOrders = 0;
     }
 
+    OrderBook() = default;
+
     // only if return True, insert new node to order book
-    static bool removeFillOrders(SortedLinkedList* orders, int* numOrders, Order* newOrder) {
+    bool removeFillOrders(SortedLinkedList* orders, int* numOrders, Order* newOrder) {
         while ( *numOrders > 0) {
             Order *currentOrder = orders->getHead()->order;
             if (newOrder->getQuantity() > currentOrder->getQuantity()) {
@@ -50,17 +52,29 @@ public:
                 orders->remove(currentOrder);
                 (*numOrders)--;
                 //TODO 2: Add PFill transaction to report for newOrder
+                addTransaction(newOrder, currentOrder->getQuantity(), 3);
+
                 //TODO 3 : Add Fill transaction to report for currentOrder
+                addTransaction(currentOrder, currentOrder->getQuantity(), 2);
+
             } else if (newOrder->getQuantity() < currentOrder->getQuantity()) {
                 currentOrder->setQuantity(currentOrder->getQuantity() - newOrder->getQuantity());
                 //TODO 4: Add Fill transaction to report for newOrder
+                addTransaction(newOrder, newOrder->getQuantity(), 2);
+
                 //TODO 5 : Add PFill transaction to report for currentOrder
+                addTransaction(currentOrder, newOrder->getQuantity(), 3);
+
                 return false;
             } else {
                 orders->remove(currentOrder);
                 (*numOrders)--;
                 //TODO 6: Add Fill transaction to report for newOrder
+                addTransaction(newOrder, newOrder->getQuantity(), 2);
+
                 //TODO 7 : Add Fill transaction to report for currentOrder
+                addTransaction(currentOrder, currentOrder->getQuantity(), 2);
+
                 return false;
             }
         }
@@ -125,6 +139,16 @@ public:
         buyOrders.print();
         std::cout << "Sell orders:" << std::endl;
         sellOrders.print();
+        std::cout << "=====================================" <<  std::endl;
+    }
+
+    void printTransactions(){
+        std::cout << "=====================================" <<  std::endl
+                    << "Transactions for " << instrument << std::endl;
+                    
+        for(auto & i : transactions){
+            i.print();
+        }
         std::cout << "=====================================" <<  std::endl;
     }
 };
